7324df4b6c4bbeeeaf0fa348d3b40fd1a8fdbb9d
Nerzul <nerkur@bk.ru>
2025-01-27 22:57:16 +0500

    fix: pdf

--- src/files/infrastructure/uploader/s3-presigned/files.module.ts
+++ src/files/infrastructure/uploader/s3-presigned/files.module.ts
@@ -37,7 +37,7 @@ const infrastructurePersistenceModule = RelationalFilePersistenceModule;
 
         return {
           fileFilter: (request, file, callback) => {
-            if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+            if (!file.originalname.match(/\.(jpg|jpeg|png|gif|pdf)$/i)) {
               return callback(
                 new UnprocessableEntityException({
                   status: HttpStatus.UNPROCESSABLE_ENTITY,
